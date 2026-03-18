1785c956b81d979205b916959afca6303f9c0a8c
Matt Rundo <mcrundo@gmail.com>
2026-03-18 00:02:53 -0400

    Adds in-memory TTL cache on Config.fetch

--- lib/omniauth/oidc/config.rb
+++ lib/omniauth/oidc/config.rb
@@ -22,9 +22,27 @@ module OmniauthOidc
       end
     end
 
-    def self.fetch(endpoint_url)
+    DEFAULT_TTL = 3600 # 1 hour
+
+    def self.fetch(endpoint_url, ttl: DEFAULT_TTL)
+      now = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+      if (cached = cache[endpoint_url]) && (now - cached[:fetched_at] < ttl)
+        return cached[:config]
+      end
+
       json = OmniAuth::Strategies::Oidc::Transport.fetch_json(endpoint_url)
-      new(json)
+      config = new(json)
+      cache[endpoint_url] = { config: config, fetched_at: now }
+      config
+    end
+
+    def self.cache
+      @cache ||= {}
+    end
+
+    def self.clear_cache!
+      @cache = {}
     end
   end
 end
